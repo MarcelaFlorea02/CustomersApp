b904ef51ac9bbc8eaef100ca895e6e02d26dda8e
marcelaflorea02 <marcela.florea02@gmail.com>
2026-03-12 09:42:34 +0200

    Add UI

---
+++ CustomersApp/LoginScreen.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CustomersApp
+{
+    partial class LoginScreen
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            loginButton = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Red;
+            label1.Location = new Point(280, 155);
+            label1.Name = "label1";
+            label1.Size = new Size(698, 149);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome!";
+            // 
+            // loginButton
+            // 
+            loginButton.Location = new Point(508, 431);
+            loginButton.Name = "loginButton";
+            loginButton.Size = new Size(188, 58);
+            loginButton.TabIndex = 1;
+            loginButton.Text = "LogIn";
+            loginButton.UseVisualStyleBackColor = true;
+            // 
+            // LoginScreen
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1193, 677);
+            Controls.Add(loginButton);
+            Controls.Add(label1);
+            Name = "LoginScreen";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button loginButton;
+    }
+}
--- CustomersApp/LoginScreen.cs
+++ CustomersApp/LoginScreen.cs
@@ -1,8 +1,8 @@
 namespace CustomersApp
 {
-    public partial class Form1 : Form
+    public partial class LoginScreen : Form
     {
-        public Form1()
+        public LoginScreen()
         {
             InitializeComponent();
         }
