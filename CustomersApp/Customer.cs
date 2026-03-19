a3d01f4260e545fa78cdb86ca7e8c7f37fb58e37
marcelaflorea02 <marcela.florea02@gmail.com>
2026-03-19 09:54:37 +0200

    Add App.Config and DBHelper class

---
+++ CustomersApp/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomersApp;
+public record Customer
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public string? Phone { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ CustomersApp/DBHelper.cs
@@ -0,0 +1,58 @@
+﻿using System.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace CustomersApp;
+
+public class DBHelper
+{
+    private readonly string _dbConnectionString;
+    public DBHelper()
+    {
+        _dbConnectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+    }
+
+    //GetAllCustomer 
+    public List<Customer> GetCustomers()
+    {
+        var customerList = new List<Customer>();
+
+        using var connection = new SqlConnection(_dbConnectionString);
+
+        const string sql = @"
+SELECT Id, FirstName, LastName, Email, Phone, CreatedAt 
+FROM Customer";
+
+        connection.Open();
+        using var command = new SqlCommand(sql, connection);
+
+        using var reader = command.ExecuteReader();
+
+        int ordId = reader.GetOrdinal("Id");
+        int ordFirstName = reader.GetOrdinal("FirstName");
+        int ordLastName = reader.GetOrdinal("LastName");
+        int ordEmail = reader.GetOrdinal("Email");
+        int ordPhone = reader.GetOrdinal("Phone");
+        int ordCreatedAt = reader.GetOrdinal("CreatedAt");
+
+        while (reader.Read())
+        {
+            var customer = new Customer()
+            {
+                Id = reader.GetInt32(ordId),
+                FirstName = reader.GetString(ordFirstName),
+                LastName = reader.GetString(ordLastName),
+                Email = reader.GetString(ordEmail),
+                Phone = reader.GetString(ordPhone),
+                CreatedAt = reader.GetDateTime(ordCreatedAt)
+            };
+            customerList.Add(customer);
+        }
+        return customerList;
+    }   
+
+    //CrateCustomer 
+
+    //UpdateCustomer 
+
+    //DeleteCustomer
+}
